b0df429a0b5a61f206519bdfb8137751f182cfea
Anusha Balakrishnan <anusha.bala28@gmail.com>
2016-09-25 12:35:51 -0700

    renamed to NeuralSystem and added some required params

--- src/basic/neural_system.py
+++ src/basic/neural_system.py
@@ -2,13 +2,13 @@ __author__ = 'anushabala'
 from system import System
 
 
-class EncDecSystem(System):
+class NeuralSystem(System):
     """
     A subclass of the System class that loads a neural model from a specified path and sends and receives message.
     """
-    def __init__(self, agent, path, kb):
+    def __init__(self, agent, kb, lexicon, model_path, schema):
         # todo this might also need some way to interface with the lexicon?
-        super(EncDecSystem, self).__init__(agent)
+        super(NeuralSystem, self).__init__(agent)
 
     def receive(self, event):
         # encode received message
