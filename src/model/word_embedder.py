672672de0dab3ef128f7551b9198199242adb509
hehe <hhe.xiy@gmail.com>
2016-12-09 12:24:54 -0800

    minor change in pad

--- src/model/word_embedder.py
+++ src/model/word_embedder.py
@@ -1,18 +1,17 @@
 import tensorflow as tf
 
 class WordEmbedder(object):
-    def __init__(self, num_symbols, embed_size, pad=None, scope=None):
+    def __init__(self, num_symbols, embed_size, scope=None):
         self.num_symbols = num_symbols
         self.embed_size = embed_size
-        self.pad = pad
         self.build_model(scope)
 
     def build_model(self, scope=None):
         with tf.variable_scope(scope or type(self).__name__):
             self.embedding = tf.get_variable('embedding', [self.num_symbols, self.embed_size])
 
-    def embed(self, inputs):
+    def embed(self, inputs, pad=None):
         embeddings = tf.nn.embedding_lookup(self.embedding, inputs)
-        if self.pad is not None:
-            embeddings = tf.select(inputs == self.pad, tf.zeros_like(embeddings), embeddings)
+        if pad is not None:
+            embeddings = tf.select(inputs == pad, tf.zeros_like(embeddings), embeddings)
         return embeddings
