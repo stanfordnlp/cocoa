a5919de765e8e9a01460f7991904436ec01ce18f
hehe <hhe.xiy@gmail.com>
2016-12-10 13:43:43 -0800

    minor

--- src/model/word_embedder.py
+++ src/model/word_embedder.py
@@ -1,17 +1,18 @@
 import tensorflow as tf
 
 class WordEmbedder(object):
-    def __init__(self, num_symbols, embed_size, scope=None):
+    def __init__(self, num_symbols, embed_size, pad=None, scope=None):
         self.num_symbols = num_symbols
         self.embed_size = embed_size
+        self.pad = pad
         self.build_model(scope)
 
     def build_model(self, scope=None):
         with tf.variable_scope(scope or type(self).__name__):
             self.embedding = tf.get_variable('embedding', [self.num_symbols, self.embed_size])
 
-    def embed(self, inputs, pad=None):
+    def embed(self, inputs, zero_pad=False):
         embeddings = tf.nn.embedding_lookup(self.embedding, inputs)
-        if pad is not None:
-            embeddings = tf.select(inputs == pad, tf.zeros_like(embeddings), embeddings)
+        if self.pad is not None and zero_pad:
+            embeddings = tf.select(inputs == self.pad, tf.zeros_like(embeddings), embeddings)
         return embeddings
