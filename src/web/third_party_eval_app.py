f471c9e8348e5d8ebb3788ba6350b349ac9e3260
Mihail Eric <mihail911@gmail.com>
2016-12-24 02:03:51 -0800

    functional server for getting frontend responses and logging to db on backend

--- src/web/third_party_eval_app.py
+++ src/web/third_party_eval_app.py
@@ -1,8 +1,10 @@
 import argparse
 import collections
 import json
+import os
 import pprint
 import random
+import sqlite3
 
 from flask import Flask, render_template, request, jsonify, session
 from gevent.wsgi import WSGIServer
@@ -14,6 +16,18 @@ parser.add_argument("--scenarios", type=str, help="path to scenarios file")
 parser.add_argument("--examples", type=str, help="path to examples file")
 args = parser.parse_args()
 
+def init_database(db_file):
+    """
+    Initalize database
+    :param db_file:  Path to db
+    :return:
+    """
+    conn = sqlite3.connect(db_file)
+    c = conn.cursor()
+    c.execute("""CREATE TABLE Responses (dialogue_num integer, humanlike_0 text, effective_0 text, strategic_0 text)""")
+    conn.commit()
+    conn.close()
+
 
 # Load examples containing UUIDs for scenarios
 examples_file = args.examples
@@ -24,18 +38,29 @@ idx_to_examples = {idx: ex for idx, ex in enumerate(examples)}
 num_examples = len(examples)
 seen_examples = set()
 
+db_path = "third_party_eval.db"
+
+# Init DB
+if os.path.exists(db_path):
+    print "DB already exists"
+else:
+    print "Creating new DB"
+    init_database(db_path)
 
 
 @app.route("/submit", methods=["GET", "POST"])
 def handle_submit():
-    global count
-    count += 1
-    current_example = session["current_example"]
-
+    backend = sqlite3.connect(db_path)
     results = request.get_json()
+    
+    pprint.pprint(results)
 
-    pprint.pprint(current_example["events"])
-
+    # Insert into dialogue
+    with backend:
+        c = backend.cursor()
+        c.execute("INSERT INTO Responses VALUES (?, ?, ?, ?)", (results["name"], results["humanlike_0"],
+                                                                results["effective_0"], results["strategic_0"]))
+        backend.commit()
 
     return jsonify(result={"status": 200})
 
@@ -53,6 +78,7 @@ def index():
 
     # Get events for example
     events = ex["events"]
+
     # Filter to only include message data
     msg_events = {}
     for idx, event in enumerate(events):
@@ -66,6 +92,7 @@ def index():
     # Will want to annotate by span of tokens
     return render_template("third_party_eval.html",
                            dialogue=msg_events,
+                           name=ex["scenario_uuid"]
                            )
 
 
