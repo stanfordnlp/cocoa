7698e34d9dbf66638305cad95cece2b110bb9111
Mihail Eric <mihail911@gmail.com>
2016-12-26 02:44:27 -0800

    more robust db handling, handling different scenarios for users, keeping track of how many evals per dialogue, logging all this info appropriately -- todo fix small dialogue out of order bug

---
+++ src/web/third_party_backend.py
@@ -0,0 +1,146 @@
+import json
+import random
+import sqlite3
+import time
+import uuid
+
+
+def get_timestamp():
+    return time.strftime("%c")
+
+
+class BackendConnection(object):
+
+    def __init__(self, db_path):
+        self.conn = sqlite3.connect(db_path)
+
+
+    def get_num_evals_completed(self, userid):
+        """
+        Get number of evals current user has completed
+        :param userid:
+        :return:
+        """
+        with self.conn:
+            c = self.conn.cursor()
+            c.execute("SELECT num_evals_completed FROM ActiveUsers WHERE user_id=?", (userid,))
+            num_evals = c.fetchone()[0]
+            return num_evals
+
+
+    def create_user_if_necessary(self, userid):
+        """
+        Adds a new user to the database if necessary
+        :param :
+        :return:
+        """
+        with self.conn:
+            cursor = self.conn.cursor()
+            now = get_timestamp()
+            cursor.execute("INSERT OR IGNORE INTO ActiveUsers VALUES (?,?,?,?)",
+                           (userid, json.dumps([]), 0, now))
+
+
+    def submit_task(self, userid, scenario_id, results, app):
+        """
+        Submit a single task and update DB appropriately.
+        :param userid: ID of user currently submitting task
+        :param scenario_id: ID of scenario being submitted
+        :param results: Dict with answers to evaluation questions
+        :param app: Storing config details
+        """
+        with self.conn:
+            cursor = self.conn.cursor()
+            now = get_timestamp()
+
+            # Update ActiveUsers to num_tasks and timestamp
+            cursor.execute("SELECT scenarios_evaluated, num_evals_completed FROM ActiveUsers WHERE user_id=?", (userid,))
+            scenarios_evaluated, prev_num_evals = cursor.fetchone()
+            updated_num_evals = prev_num_evals + 1
+
+            # Update scenarios evaluated
+            scenarios_evaluated = json.loads(scenarios_evaluated)
+            scenarios_evaluated.append(scenario_id)
+
+            cursor.execute("UPDATE ActiveUsers SET num_evals_completed=?, scenarios_evaluated=?, timestamp=? WHERE user_id=?", (updated_num_evals,
+                                                                                                                               json.dumps(scenarios_evaluated), now, userid))
+
+            # Record answers to evaluation
+            cursor.execute("INSERT INTO Responses VALUES (?,?,?,?,?,?,?,?,?,?)", (scenario_id, userid, results["humanlike_0"],
+                                                                                  results["correct_0"], results["strategic_0"],
+                                                                                  results["fluent_0"], results["humanlike_1"],
+                                                                                  results["correct_1"], results["strategic_1"],
+                                                                                  results["fluent_1"]))
+            try:
+                # Update number of evals on dialogue
+                cursor.execute("SELECT num_evals FROM ActiveDialogues WHERE scenario_id=?", (scenario_id,))
+                num_evals = cursor.fetchone()[0]
+                updated_num_evals = num_evals + 1
+                # Dialogue has been evaluated requisite number of times so move to CompletedDialogues
+                if updated_num_evals == app.config["num_evals_per_dialogue"]:
+                    cursor.execute("INSERT INTO CompletedDialogues VALUES (?,?,?)",
+                                   (scenario_id, updated_num_evals, now))
+                    cursor.execute("DELETE FROM ActiveDialogues WHERE scenario_id=?", (scenario_id,))
+                else:
+                    # Update number of evals completed for dialogue
+                    cursor.execute("UPDATE ActiveDialogues SET num_evals=? WHERE scenario_id=?", (updated_num_evals, scenario_id))
+
+            except TypeError as e:
+                print "Catching error: ", e
+
+
+    def get_finished_info(self, userid):
+        """
+        Get info related to a user once they have finished requisite number of tasks
+        :param userid:
+        :param from_mturk:
+        :return:
+        """
+        mturk_code = "MTURK_TASK_{}".format(str(uuid.uuid4().hex))
+        with self.conn:
+            cursor = self.conn.cursor()
+            now = get_timestamp()
+            cursor.execute("SELECT num_evals_completed FROM ActiveUsers WHERE user_id=?", (userid,))
+            num_evals = cursor.fetchone()[0]
+            cursor.execute("INSERT INTO CompletedUsers VALUES (?,?,?,?)", (userid, mturk_code, now, num_evals))
+            # Reset number of completed tasks for given user to 0
+            cursor.execute("UPDATE ActiveUsers SET num_evals_completed=?, timestamp=? WHERE user_id= ?", (0, now, userid))
+            return mturk_code
+
+
+    def get_dialogue(self, userid):
+        """
+        Get an unfinished dialogue to display for user. Return necessary information
+        for displaying on frontend
+        :param userid:
+        :return:
+        """
+        with self.conn:
+            cursor = self.conn.cursor()
+
+            try:
+                cursor.execute("SELECT scenario_id, events, column_names, agent0_kb, agent1_kb FROM ActiveDialogues")
+                dialogues = cursor.fetchall()
+
+                cursor.execute("SELECT scenarios_evaluated FROM ActiveUsers WHERE user_id=?", (userid,))
+                scenarios_evaluated = cursor.fetchone()
+                print scenarios_evaluated
+                scenarios_evaluated = set(json.loads(scenarios_evaluated[0]))
+                print "EVAL: ", scenarios_evaluated
+                selected = None
+                for d in dialogues:
+                    # Found a dialogue not previously shown to user
+                    if d[0] not in scenarios_evaluated and d[1] != "{}":
+                        selected = d
+                        break
+                #print "Selected: ", selected
+                return selected
+            except Exception as e:
+                print "Error sampling from DB: ", e
+                return None
+
+
+    def close(self):
+        self.conn.close()
+        self.conn = None
+
--- src/web/third_party_eval_app.py
+++ src/web/third_party_eval_app.py
@@ -5,9 +5,11 @@ import os
 import pprint
 import random
 import sqlite3
+import uuid
 
-from flask import Flask, render_template, request, jsonify, session
+from flask import Flask, render_template, request, jsonify, session, g
 from gevent.wsgi import WSGIServer
+from third_party_backend import BackendConnection
 
 app = Flask(__name__)
 
@@ -15,42 +17,146 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--scenarios", type=str, help="path to scenarios file")
 parser.add_argument("--examples", type=str, help="path to examples file")
 parser.add_argument("--port", type=int, help="port to launch app on")
+parser.add_argument("--num-evals-per-worker", type=int, help="number of evaluations per worker before redeeming code")
+parser.add_argument("--num-evals-per-dialogue", type=int, help="number of evaluations per dialogue ")
 args = parser.parse_args()
 
-def init_database(db_file):
+
+
+def init_database(db_path):
     """
     Initalize database
-    :param db_file:  Path to db
+    :param db_path:  Path to db
     :return:
     """
-    conn = sqlite3.connect(db_file)
+    conn = sqlite3.connect(db_path)
     c = conn.cursor()
-    c.execute("""CREATE TABLE Responses (dialogue_num integer, humanlike_0 text, correct_0 text, strategic_0 text, fluent_0 text,
+    c.execute("""CREATE TABLE Responses (scenario_id text, user_id text, humanlike_0 text, correct_0 text, strategic_0 text, fluent_0 text,
               humanlike_1 text, correct_1 text, strategic_1 text, fluent_1 text)""")
+    c.execute("""CREATE TABLE ActiveDialogues (scenario_id text unique, events text, column_names text, agent0_kb text, agent1_kb text, num_evals integer)""")
+    c.execute("""CREATE TABLE CompletedDialogues(scenario_id text, num_evals integer, timestamp text)""")
+    c.execute("""CREATE TABLE ActiveUsers (user_id text unique, scenarios_evaluated text, num_evals_completed integer, timestamp text)""")
+    c.execute("""CREATE TABLE CompletedUsers (user_id text, mturk_code text, timestep text, num_evals_completed integer)""")
     conn.commit()
     conn.close()
 
 
-# Load examples containing UUIDs for scenarios
-examples_file = args.examples
-with open(examples_file, "r") as f:
-    examples = json.load(f)
+def init_dialogues(db_path):
+    """
+    Initialize all dialogues if not previously initialized
+    :param db_path: Path to db
+    :return:
+    """
+    conn = sqlite3.connect(db_path)
+    c = conn.cursor()
 
-# Load scenarios containing KB info
-scenarios_json = args.scenarios
-with open(scenarios_json, "r") as f:
-    scenarios_info = json.load(f)
+    # Load examples containing UUIDs for scenarios
+    examples_file = args.examples
+    with open(examples_file, "r") as f:
+        examples = json.load(f)
 
-# Form mapping from uuid -> scenario
-uuid_to_scenario = collections.defaultdict(dict)
-for scenario in scenarios_info:
-    uuid = scenario["uuid"]
-    uuid_to_scenario[uuid] = scenario
+    # Load scenarios containing KB info
+    scenarios_json = args.scenarios
+    with open(scenarios_json, "r") as f:
+        scenarios_info = json.load(f)
 
-# Mapping from id -> examples
-idx_to_examples = {idx: ex for idx, ex in enumerate(examples)}
-num_examples = len(examples)
-seen_examples = set()
+    # Form mapping from uuid -> scenario
+    uuid_to_scenario = collections.defaultdict(dict)
+    for scenario in scenarios_info:
+        uuid = scenario["uuid"]
+        uuid_to_scenario[uuid] = scenario
+
+    # Mapping from id -> examples
+    idx_to_examples = {idx: ex for idx, ex in enumerate(examples)}
+
+    for idx, ex in idx_to_examples.iteritems():
+        uuid = ex["scenario_uuid"]
+        scenario = uuid_to_scenario[uuid]
+        kbs = scenario["kbs"]
+        agent0_kb = kbs[0] #
+        agent1_kb = kbs[1] #
+        column_names = agent1_kb[0].keys() #
+
+        # Get events for example
+        events = ex["events"]
+
+        # Filter to only include message data
+        msg_events = {} #
+        for idx, event in enumerate(events):
+            if event["action"] == "message":
+                msg_events[idx] = event
+
+        c.execute("""INSERT OR IGNORE INTO ActiveDialogues VALUES (?,?,?,?,?,?) """,
+            (uuid, json.dumps(msg_events), json.dumps(column_names), json.dumps(agent0_kb),
+            json.dumps(agent1_kb), 0))
+
+    conn.commit()
+    conn.close()
+
+
+def get_backend():
+    backend = getattr(g, '_backend', None)
+    if backend is None:
+        print "Creating backend..."
+        backend = g._backend = BackendConnection(app.config["db_path"])
+    return backend
+
+
+def set_or_get_userid():
+    if "sid" in session and session["sid"]:
+        return userid()
+    if "sid" not in session or not session["sid"]:
+        print "New user created!"
+        session["sid"] = str(uuid.uuid4())[:12]
+    print "Session ID: ", session["sid"]
+    return session["sid"]
+
+
+def userid():
+    return session["sid"]
+
+
+@app.route("/submit", methods=["GET", "POST"])
+def handle_submit():
+    results = request.get_json()
+
+    pprint.pprint(results)
+
+    backend = get_backend()
+    print "USER ID: ", userid()
+    print "Scenario ID: ", results["uuid"]
+
+    backend.submit_task(userid(), results["uuid"], results, app)
+
+
+    return jsonify(result={"status": 200})
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    _ = set_or_get_userid()
+    backend = get_backend()
+    backend.create_user_if_necessary(userid())
+    num_evals_completed = backend.get_num_evals_completed(userid())
+    if num_evals_completed < app.config["num_evals_per_worker"]:
+        dialogue = backend.get_dialogue(userid())
+
+        # TODO: Fix error in order of msg event displays
+        return render_template("third_party_eval.html",
+                               dialogue=json.loads(dialogue[1]),
+                               uuid=dialogue[0],
+                               column_names=json.loads(dialogue[2]),
+                               kb0=json.loads(dialogue[3]),
+                               kb1=json.loads(dialogue[4])
+                               )
+    else:
+        mturk_code = backend.get_finished_info(userid())
+        return render_template("third_party_eval_finished.html",
+                               mturk_code=mturk_code,
+                               finished_message="YOU HAVE FINISHED THE HIT!")
+
+
 
 db_path = "third_party_eval.db"
 
@@ -61,70 +167,14 @@ else:
     print "Creating new DB"
     init_database(db_path)
 
-
-@app.route("/submit", methods=["GET", "POST"])
-def handle_submit():
-    backend = sqlite3.connect(db_path)
-    results = request.get_json()
-
-    pprint.pprint(results)
-
-    # Insert into dialogue
-    with backend:
-        c = backend.cursor()
-        c.execute("INSERT INTO Responses VALUES (?,?,?,?,?,?,?,?,?)", (results["name"], results["humanlike_0"],
-                                                                results["correct_0"], results["strategic_0"],
-                                                                results["fluent_0"], results["humanlike_1"],
-                                                                results["correct_1"], results["strategic_1"],
-                                                                results["fluent_1"]))
-        backend.commit()
-
-    return jsonify(result={"status": 200})
-
-
-@app.route('/')
-@app.route('/index')
-def index():
-    # Sample random example
-    while True:
-        ex_idx = random.sample(range(num_examples), 1)
-        if ex_idx[0] not in seen_examples:
-            break
-    ex = idx_to_examples[ex_idx[0]]
-    seen_examples.add(ex_idx[0])
-
-    uuid = ex["scenario_uuid"]
-    scenario = uuid_to_scenario[uuid]
-    kbs = scenario["kbs"]
-    agent0_kb = kbs[0]
-    agent1_kb = kbs[1]
-    column_names = agent1_kb[0].keys()
-
-    # Get events for example
-    events = ex["events"]
-
-    # Filter to only include message data
-    msg_events = {}
-    for idx, event in enumerate(events):
-        if event["action"] == "message":
-            msg_events[idx] = event
-
-
-    # Keep track of current examples being annotated
-    session["current_example"] = ex
-
-    # Will want to annotate by span of tokens
-    return render_template("third_party_eval.html",
-                           dialogue=msg_events,
-                           name=ex["scenario_uuid"],
-                           column_names=column_names,
-                           kb0=agent0_kb,
-                           kb1=agent1_kb
-                           )
+init_dialogues(db_path)
 
 
 # Launch server
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+app.config["num_evals_per_worker"] = args.num_evals_per_worker
+app.config["num_evals_per_dialogue"] = args.num_evals_per_dialogue
+app.config["db_path"] = db_path
 
 server = WSGIServer(('', args.port), app)
 server.serve_forever()
