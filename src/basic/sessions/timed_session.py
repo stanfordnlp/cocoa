46ee05e12e55d1b4d0451847c2a486c354834b1b
He He <hhe.xiy@gmail.com>
2016-12-29 16:28:52 -0500

    use time.time()

--- src/basic/sessions/timed_session.py
+++ src/basic/sessions/timed_session.py
@@ -1,6 +1,6 @@
 __author__ = 'anushabala'
 from session import Session
-import datetime
+import time
 import random
 from collections import deque
 
@@ -12,15 +12,15 @@ class TimedSessionWrapper(Session):
     the wrapper will add a delay to the responses sent by the session in order to simulate human typing/action rates.
     """
     CHAR_RATE = 10
-    EPSILON = 1500
-    SELECTION_DELAY = 1000
-    REPEATED_SELECTION_DELAY = 10000
-    PATIENCE = 4000
+    EPSILON = 1.5
+    SELECTION_DELAY = 1
+    REPEATED_SELECTION_DELAY = 10
+    PATIENCE = 2
 
     def __init__(self, agent, session):
         super(TimedSessionWrapper, self).__init__(agent)
         self.session = session
-        self.last_message_timestamp = datetime.datetime.now()
+        self.last_message_timestamp = time.time()
         self.queued_event = deque()
         self.prev_action = None
         self.received = False
@@ -30,18 +30,16 @@ class TimedSessionWrapper(Session):
         # join and leave events
         if not (event.action == 'select' or event.action == 'message'):
             return
-        self.last_message_timestamp = datetime.datetime.now()
+        self.last_message_timestamp = time.time()
         self.session.receive(event)
         self.received = True
         self.num_utterances = 0
         self.queued_event.clear()
 
     def send(self):
-        #if self.received is False and (self.last_message_timestamp + datetime.timedelta(milliseconds=random.uniform(self.PATIENCE/2., self.PATIENCE*2.)) > datetime.datetime.now() or self.prev_action == 'select'):
-        #    return None
         if (self.received is False and self.prev_action == 'select') or \
             self.num_utterances == 2 or \
-            (self.received is False and self.last_message_timestamp + datetime.timedelta(milliseconds=random.uniform(self.PATIENCE/2., self.PATIENCE*2.)) > datetime.datetime.now()):
+            (self.received is False and self.last_message_timestamp + random.uniform(self.PATIENCE, self.PATIENCE*2.) > time.time()):
             return None
 
 	if len(self.queued_event) == 0:
@@ -51,7 +49,7 @@ class TimedSessionWrapper(Session):
         if event is None:
             return self.queued_event.popleft()
         if event.action == 'message':
-            delay = float(len(event.data)) / self.CHAR_RATE * 1000 + random.uniform(0, self.EPSILON)
+            delay = float(len(event.data)) / self.CHAR_RATE + random.uniform(0, self.EPSILON)
         elif event.action == 'select':
             delay = self.SELECTION_DELAY + random.uniform(0, self.EPSILON)
             if self.prev_action == 'select':
@@ -60,12 +58,13 @@ class TimedSessionWrapper(Session):
             # unsupported event action type?
             return None
 
-        if self.last_message_timestamp + datetime.timedelta(milliseconds=delay) > datetime.datetime.now():
+        if self.last_message_timestamp + delay > time.time():
             return None
         else:
-            self.last_message_timestamp = datetime.datetime.now()
             event = self.queued_event.popleft()
             self.prev_action = event.action
             self.received = False
             self.num_utterances += 1
+            self.last_message_timestamp = time.time()
+            event.time = str(self.last_message_timestamp)
             return event
