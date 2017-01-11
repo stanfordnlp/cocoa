1b511f3d5141520063ea431b277f468d968bab3c
hehe <hhe.xiy@gmail.com>
2017-01-11 11:36:35 -0800

    add max turns

--- src/scripts/generate_dataset.py
+++ src/scripts/generate_dataset.py
@@ -26,6 +26,7 @@ parser.add_argument('--model-path', help='Path to model (used for neural agents)
 parser.add_argument('--scenario-offset', default=0, type=int, help='Number of scenarios to skip at the beginning')
 parser.add_argument('--remove-fail', default=False, action='store_true', help='Remove failed dialogues')
 parser.add_argument('--stats-file', default='stats.json', help='Path to save json statistics (dataset, training etc.) file')
+parser.add_argument('--max-turns', default=100, type=int, help='Maximum number of turns')
 parser.add_argument('--fact-check', default=False, action='store_true', help='Check if the utterance is true given the KB. Only work for simulated data.')
 add_scenario_arguments(parser)
 add_lexicon_arguments(parser)
@@ -70,7 +71,7 @@ agents = [get_system(name) for name in args.agents]
 num_examples = args.scenario_offset
 
 summary_map = {}
-def generate_examples(description, examples_path, max_examples, remove_fail):
+def generate_examples(description, examples_path, max_examples, remove_fail, max_turns):
     global num_examples
     examples = []
     num_failed = 0
@@ -78,7 +79,7 @@ def generate_examples(description, examples_path, max_examples, remove_fail):
         scenario = scenario_db.scenarios_list[num_examples % len(scenario_db.scenarios_list)]
         sessions = [agents[0].new_session(0, scenario.kbs[0]), agents[1].new_session(1, scenario.kbs[1])]
         controller = Controller(scenario, sessions)
-        ex = controller.simulate()
+        ex = controller.simulate(max_turns)
         if ex.outcome['reward'] == 0:
             num_failed += 1
             if remove_fail:
@@ -99,6 +100,6 @@ def generate_examples(description, examples_path, max_examples, remove_fail):
             logstats.add('bot_chat', results)
 
 if args.train_max_examples:
-    generate_examples('train', args.train_examples_paths[0], args.train_max_examples, args.remove_fail)
+    generate_examples('train', args.train_examples_paths[0], args.train_max_examples, args.remove_fail, args.max_turns)
 if args.test_max_examples:
-    generate_examples('test', args.test_examples_paths[0], args.test_max_examples, args.remove_fail)
+    generate_examples('test', args.test_examples_paths[0], args.test_max_examples, args.remove_fail, args.max_turns)
