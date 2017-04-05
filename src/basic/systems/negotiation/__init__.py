b3031226a0189ddbe7b2a10b706b65d68a5ced8c
He He <hhe.xiy@gmail.com>
2017-04-05 22:27:00 +0800

    cmd session

--- src/basic/systems/negotiation/__init__.py
+++ src/basic/systems/negotiation/__init__.py
@@ -1,5 +1,6 @@
-from rulebased_system import RulebasedSystem
 from src.basic.price_tracker import PriceTracker
+from rulebased_system import RulebasedSystem
+from cmd_system import CmdSystem
 
 def add_system_arguments(parser):
     return
@@ -8,5 +9,7 @@ def get_system(name, args, schema=None, timed=False):
     lexicon = PriceTracker()
     if name == 'rulebased':
         return RulebasedSystem(lexicon, timed)
+    elif name == 'cmd':
+        return CmdSystem()
     else:
         raise ValueError('Unknown system %s' % name)
