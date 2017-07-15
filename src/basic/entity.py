564e1697b588dde16921e9313bae561d0d0bb8d0
hehe <hhe.xiy@gmail.com>
2017-07-14 21:49:11 -0700

    add __str__ to Entity

--- src/basic/entity.py
+++ src/basic/entity.py
@@ -1,7 +1,14 @@
 from collections import namedtuple
 
-CanonicalEntity = namedtuple('CanonicalEntity', ['value', 'type'])
-Entity = namedtuple('Entity', ['surface', 'canonical'])
+class CanonicalEntity(namedtuple('CanonicalEntity', ['value', 'type'])):
+    __slots__ = ()
+    def __str__(self):
+        return '[%s]' % str(self.value)
+
+class Entity(namedtuple('Entity', ['surface', 'canonical'])):
+    __slots__ = ()
+    def __str__(self):
+        return '[%s]' % str(self.surface)
 
 def is_entity(x):
     return isinstance(x, Entity) or isinstance(x, CanonicalEntity)
