6e41911983c61facc92e207af2e17708c2db1b2c
hehe <hhe.xiy@gmail.com>
2017-09-12 22:54:48 -0700

    turk eval pipeline

---
+++ cocoa/turk/eval_data.py
@@ -0,0 +1,145 @@
+import random
+
+from cocoa.core.util import read_json, write_json
+
+from utils import xml_safe
+
+def add_eval_data_arguments(parser):
+    parser.add_argument('--system-outputs', nargs='*', help='Path to JSON data or raw generation results in forms of (system, path, ...)')
+    parser.add_argument('--eval-data', help='Path to saved evaluation data in JSON')
+    parser.add_argument('--eval-data-output', help='Path to write evaluation data in JSON')
+    parser.add_argument('--num-context', default=3, help='Number of utterances to display in the context')
+
+class EvalData(object):
+    def __init__(self, data):
+        self.data = data
+
+    def dump(self, output):
+        print 'Dumping data to {}'.format(output)
+        write_json(self.data, output)
+
+    def sample_examples(self, num_context, evaluated=set()):
+        """Randomly sample num_context examples to evaluate.
+
+        Args:
+            num_context (int)
+            evaluated (set): set of example ids that have been evaluated
+
+        Returns:
+            examples (list[(qid, context, response)]): tuples that can be used to contruct questions by EvalTask.
+
+        """
+        example_ids = [x for x in self.data.keys() if not x in evaluated]
+        if num_context > len(example_ids):
+            print 'WARNING: wanted {} examples, only has {}'.format(num_context, len(example_ids))
+            num_context = len(example_ids)
+        selected_ids = random.sample(example_ids, num_context)
+        examples = []
+        for id_ in selected_ids:
+            responses = self.data[id_]['responses']
+            context = self.data[id_]['context']
+            for system, response in responses.iteritems():
+                qid = '{system}-{ex_id}'.format(system=system, ex_id=id_)
+                examples.append((qid, context, response))
+        return examples
+
+    @classmethod
+    def valid_example(cls, example, num_context_utterances):
+        """Filter some examples that should not be evaluated.
+
+        Args:
+            example (dict): an entry in system outputs (see read_system_responses)
+            num_context_utterances (int)
+
+        Returns:
+            valid (bool)
+
+        """
+        if len(example['prev_turns']) < num_context_utterances:
+            return False
+        return True
+
+    @classmethod
+    def read_system_responses(cls, system, path, num_context_utterances, data):
+        """Read system responses and update the database.
+
+        Args:
+            system (str): system name
+            path (str): a JSON file containing system outputs.
+                |-[]
+                 |-ex_id (str): unique id that identifies a context-reference pair
+                 |-prev_turns (list)
+                 |-reference
+                 |-response
+            num_context_utterances (int)
+            data (dict): see from_file.
+
+        """
+        examples = read_json(path)
+        for ex in examples:
+            if not cls.valid_example(ex, num_context_utterances):
+                continue
+            qid = ex['ex_id']
+            context_turns = ex['prev_turns'][-1*num_context_utterances:]
+            agent_names = cls.get_agent_name(context_turns + [ex['reference']])
+            context = []
+            for i, u in enumerate(context_turns):
+                u = cls.process_utterance(u, role=agent_names[i])
+                if len(u[1]) > 0:
+                    context.append(u)
+            reference = cls.process_utterance(ex['reference'], role=agent_names[-1])
+            response = cls.process_utterance(ex['response'], role=agent_names[-1])
+            if not (len(reference) and len(response) and len(context)):
+                continue
+            if qid not in data:
+                data[qid] = {
+                        'context': context,
+                        'responses': {}
+                        }
+            assert system not in data[qid]['responses']
+            data[qid]['responses'][system] = response
+            data[qid]['responses']['reference'] = reference
+
+    @classmethod
+    def get_agent_name(cls, turns):
+        num_turns = len(turns)
+        return ['A' if i % 2 == 0 else 'B' for i in num_turns]
+
+    @classmethod
+    def process_utterance(cls, utterance, role=''):
+        """Process the utterance to a form to be displayed to turkers.
+
+        Returns:
+            agent, utterance: (str, str)
+
+        """
+        return (role, xml_safe(utterance))
+
+    @classmethod
+    def from_json(cls, data_path):
+        """Construct from dumped data.
+        """
+        data = read_json(data_path)
+        return cls(data)
+
+    @classmethod
+    def from_file(cls, paths, num_context_utterances=3):
+        """Read context-response pairs from system outputs.
+
+        Args:
+            paths (list[tuple]): [(system, path)]
+            num_context_utterances (int): number of utterances to use as context
+
+        Returns:
+            data (dict):
+                |-qid (str)
+                 |-context ([(agent, utterance)])
+                 |-responses
+                  |-system (str)
+                   |-response ((agent, utterance))
+
+        """
+        data = {}
+        for system, path in paths:
+            cls.read_system_responses(system, path, num_context_utterances, data)
+        return cls(data)
--- cocoa/turk/task.py
+++ cocoa/turk/task.py
@@ -1,12 +1,22 @@
 import sys
+import os
 
-from boto.mturk.question import QuestionContent, Question, QuestionForm, Overview, AnswerSpecification, SelectionAnswer, FormattedContent, FreeTextAnswer
+from boto.mturk.question import QuestionContent, Question, QuestionForm, Overview, AnswerSpecification, SelectionAnswer, FormattedContent, FreeTextAnswer, HTMLQuestion
 
 from cocoa.core.util import read_json, write_json
 from utils import default_qualifications, xml_safe
 
+def add_turk_task_arguments(parser):
+    parser.add_argument('--overall-template', help='Path to HTML templates')
+    parser.add_argument('--question-template', help='Path to HTML templates')
+    parser.add_argument('--instructions', help='Path to instructions')
+    parser.add_argument('--num-assignments-per-hit', type=int, default=3, help='How many lables do we want on each example')
+    parser.add_argument('--num-questions-per-hit', type=int, default=10, help='How many questions in a hit')
+    parser.add_argument('--reward-per-hit', type=float, default=0.10, help='Payment per hit in dollar')
+    parser.add_argument('--hit-db', default='hits.json', help='Path to JSON file that saves hits info')
+
 class Task(object):
-    def __init__(self, mtc=None, title=None, description=None, keywords=None, reward=None, max_assignments=1, qualifications=None, duration=60, approval_delay=3, lifetime=30):
+    def __init__(self, mtc=None, title=None, description=None, keywords=None, reward=None, max_assignments=1, qualifications=None, duration=60, approval_delay=3, lifetime=30, db_path='hits.json'):
         """Initialize a Turk task with basic properties.
 
         Args:
@@ -20,6 +30,7 @@ class Task(object):
             duration (int): in minutes.
             approval_delay (int): in days.
             lifetime (int): in days.
+            db_path (str): file for saving hit_ids and results.
 
         """
         self.title = title
@@ -38,43 +49,111 @@ class Task(object):
         hit_type_ids = self.mtc.register_hit_type(self.title, self.description, self.reward, self.duration, self.keywords, self.approval_delay, self.qualifications)
         self.hit_type_id = hit_type_ids[0].HITTypeId
 
+        self.db_path = db_path
+        self.load_db()
+
+    def load_db(self):
+        db_path = self.db_path
+        if os.path.exists(db_path):
+            self.db = read_json(db_path)
+        else:
+            self.db = {}
+
+    def dump_db(self):
+        write_json(self.db, self.db_path)
+        print 'HIT results dumped to {}'.format(self.db_path)
+
     def create_hit(self, question):
         hit = self.mtc.create_hit(hit_type=self.hit_type_id, question=question, lifetime=self.lifetime, max_assignments=self.max_assignments)
         return hit[0].HITId
 
-    # TODO: save hit_ids when creating!
-    def get_reviewable_results(self, N=100):
-        current_page = 1
-        page_size=100
-        my_hits = []
-        while True:
-            search_rs = self.mtc.search_hits(page_size=page_size, page_number=current_page, sort_direction='Descending')
-            for hit in search_rs:
-                if hit.HITTypeId == self.hit_type_id and hit.HITStatus == 'Reviewable':
-                    my_hits.append(hit)
-            if len(my_hits) >= N or current_page >= 10:
-                break
-            else:
-                current_page += 1
+    def launch_hits(self, questions):
+        decision = raw_input('About to create {} HITs that costs ${}. Continue? [Y/N]'.format(len(questions), self.reward * len(questions) * self.max_assignments))
+        if decision == 'Y':
+            for q in questions:
+                hit_id = self.create_hit(q)
+                self.db[hit_id] = {}
+            self.dump_db()
+            print "Your HIT has been created. You can see it at this link:"
+            print "https://workersandbox.mturk.com/mturk/preview?groupId={}".format(self.hit_type_id)
+        else:
+            print 'Abort'
 
+    def get_reviewable_results(self):
         results = []
-        for hit in my_hits:
-            hit_id = hit.HITId
-            result = self.mtc.get_assignments(hit_id)
-            assignment = result[0]
-            worker_id = assignment.WorkerId
-            answers = []
-            for answer in assignment.answers[0]:
-                answers.append({'qid': answer.qid, 'answer': answer.fields[0]})
-            results.append({
-                'hit_id': hit_id,
-                'worker_id': worker_id,
-                'answers': answers,
-                })
-        return results
-
+        for hit_id, hit_info in self.db.iteritems():
+            assignments = self.mtc.get_assignments(hit_id)
+            if assignments:
+                for assignment in assignments:
+                    answers = []
+                    for answer in assignment.answers[0]:
+                        answers.append({'qid': answer.qid, 'answer': answer.fields[0]})
+                    hit_info[assignment.AssignmentId] = {
+                            'worker_id': assignment.WorkerId,
+                            'answers': answers,
+                            }
+        self.dump_db()
 
 class EvalTask(Task):
+    def utterance_formatter(self, linebreak=False, color=None):
+        template = '{agent}: {text}'
+        if color:
+            template = '<span style=color:{color}>{temp}</span>'.format(color=color, temp=template)
+        if linebreak:
+            template = '{}<br>'.format(template)
+        return template
+
+    def format_context(self, context):
+        utterance_formatter = '{agent}: {text}'
+        display_context = []
+        for i, (agent, utterance) in enumerate(context):
+            color = 'red' if i % 2 == 0 else 'green'
+            display_context.append(self.utterance_formatter(linebreak=True, color=color).format(agent=agent, text=utterance))
+        return ''.join(display_context)
+
+
+class HTMLEvalTask(EvalTask):
+    def __init__(self, question_template_path=None, overall_template_path=None, instructions_path=None, question_title='Dialogue evaluation', **kwargs):
+        super(HTMLEvalTask, self).__init__(**kwargs)
+        with open(overall_template_path, 'r') as fin:
+            self.overall_template = fin.read().strip()
+        with open(question_template_path, 'r') as fin:
+            self.question_template = fin.read().strip()
+        with open(instructions_path, 'r') as fin:
+            self.instructions = fin.read().strip()
+        self.title = question_title
+
+    def create_questions(self, questions):
+        hit_questions = []
+        for question_group in questions:
+            html_questions = self.create_question_group(question_group)
+            html_hit = self.overall_template.format(
+                    title=self.title,
+                    instructions=self.instructions,
+                    questions=html_questions,
+                    )
+            html_hit = HTMLQuestion(html_hit, 600)
+            hit_questions.append(html_hit)
+        return hit_questions
+
+    def create_question_group(self, questions):
+        html_questions = []
+        for qid, context, response in questions:
+            html_context = self.format_context(context)
+            agent, utterance = response
+            html_response = self.utterance_formatter().format(agent=agent, text=utterance.encode('utf-8'))
+            html_question = self.question_template.format(
+                qid=qid,
+                context=html_context,
+                response=html_response,
+                )
+            html_questions.append(html_question)
+        return '\n'.join(html_questions)
+
+
+################ DEPRECATED ################
+
+class QuestionFormEvalTask(EvalTask):
     ratings = [
             ('Very unlikely', -2),
             ('Not likely', -1),
@@ -83,27 +162,19 @@ class EvalTask(Task):
             ('Very likely', 2),
             ]
 
-    def utterance_formatter(self, bold=False, linebreak=False):
-        template = '{role}: {text}'
-        if bold:
-            template = '<b>{}</b>'.format(template)
-        if linebreak:
-            template = '{}<br></br>'.format(template)
-        return template
-
     def build_question(self, qid, context, response):
         qc = QuestionContent()
 
-        utterance_formatter = '{role}: {text}'
+        utterance_formatter = '{agent}: {text}'
         #qc.append_field('Text', 'Dialogue context:')
         display_context = []
-        for role, utterance in context:
-            display_context.append(self.utterance_formatter(linebreak=True).format(role=role, text=utterance))
+        for agent, utterance in context:
+            display_context.append(self.utterance_formatter(linebreak=True).format(agent=agent, text=utterance))
         qc.append(FormattedContent(''.join(display_context)))
 
         #qc.append_field('Text', 'Response:')
-        role, utterance = response
-        qc.append(FormattedContent(self.utterance_formatter(bold=True).format(role=role, text=utterance)))
+        agent, utterance = response
+        qc.append(FormattedContent(self.utterance_formatter().format(agent=agent, text=utterance)))
 
         qc.append_field('Text', 'Please rate how likely the response (in bold) continues from the conversation above.')
 
@@ -111,30 +182,30 @@ class EvalTask(Task):
                 selections=self.ratings,
                 type='text', other=False)
 
-	q = Question(identifier=qid,
-              content=qc,
-              answer_spec=AnswerSpecification(ans),
-              is_required=True)
+        q = Question(identifier=qid,
+                content=qc,
+                answer_spec=AnswerSpecification(ans),
+                is_required=True)
 
-	return q
+        return q
 
     def build_comment(self):
         qc = QuestionContent()
         qc.append_field('Text', 'Comments:')
         ans = FreeTextAnswer()
-	q = Question(identifier='comment',
+        q = Question(identifier='comment',
               content=qc,
               answer_spec=AnswerSpecification(ans),
               is_required=False)
         return q
 
     def build_question_form(self, questions):
-	question_form = QuestionForm()
-	question_form.append(self.build_overview())
+        question_form = QuestionForm()
+        question_form.append(self.build_overview())
         for qid, context, response in questions:
-	    question_form.append(self.build_question(qid, context, response))
-        question_form.append(self.build_comment())
-	return question_form
+            question_form.append(self.build_question(qid, context, response))
+            question_form.append(self.build_comment())
+        return question_form
 
     def build_overview(self,):
         overview = Overview()
