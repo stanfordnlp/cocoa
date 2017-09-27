0ea367d72c3262d8a00485ee3d192a6ce4ac8515
Derek Chen <derekchen14@gmail.com>
2017-09-27 14:44:10 -0700

    Update the shell scripts for running examples and their corresponding instructions

---
+++ fb-negotiation/run.sh
@@ -0,0 +1,12 @@
+# PYTHONPATH=. python ../scripts/generate_dataset.py \
+#     --schema-path data/bookhatball-schema.json \
+#     --scenarios-path data/toy-scenarios.json \
+#     --train-examples-paths data/toy-transcripts.json \
+#     --train-max-examples 1 --test-max-examples 0 --max-turns 20 \
+#     --agents rulebased cmd \
+#     --verbose --random-seed 8
+
+PYTHONPATH=. python web/chat_app.py --port 1414 \
+    --schema-path data/bookhatball-schema.json \
+    --config web/app_params.json --scenarios-path data/toy-scenarios.json \
+    --output web/output
--- negotiation/run.sh
+++ negotiation/run.sh
@@ -1,10 +1,7 @@
-PYTHONPATH=. python ../scripts/generate_dataset.py \
-    --schema-path data/bookhatball-schema.json \
-    --scenarios-path data/toy-scenarios.json \
-    --train-examples-paths data/toy-transcripts.json \
-    --train-max-examples 1 --test-max-examples 0 --max-turns 20 \
-    --agents rulebased cmd \
-    --verbose --random-seed 8
+PYTHONPATH=. python web/chat_app.py --port 5000 \
+    --schema-path data/craigslist-schema.json \
+    --config web/app_params.json --scenarios-path data/sample-scenarios.json \
+    --output web/output --price-tracker price-tracker.pkl
 
 # PYTHONPATH=. python ../scripts/generate_dataset.py \
 #     --schema-path data/craigslist-schema.json \
