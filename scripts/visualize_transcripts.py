f22a76b40e70979e7911b8031c3f15f6fd0b6f81
Derek Chen <derekchen14@gmail.com>
2017-10-08 11:31:10 -0700

    Just reformat visual transcripts to fit on 80 chars per line

--- scripts/visualize_transcripts.py
+++ scripts/visualize_transcripts.py
@@ -7,19 +7,28 @@ from analysis.html_visualizer import HTMLVisualizer
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument('--survey-transcripts', nargs='+', help='Path to directory containing evaluation transcripts')
-    parser.add_argument('--dialogue-transcripts', nargs='+', help='Path to directory containing dialogue transcripts')
-    parser.add_argument('--worker-ids', nargs='+', help='Path to json file containing chat_id to worker_id mappings')
-    parser.add_argument('--summary', default=False, action='store_true', help='Summarize human ratings')
-    parser.add_argument('--hist', default=False, action='store_true', help='Plot histgram of ratings')
-    parser.add_argument('--html-visualize', action='store_true', help='Output html files')
+    parser.add_argument('--survey-transcripts', nargs='+',
+            help='Path to directory containing evaluation transcripts')
+    parser.add_argument('--dialogue-transcripts', nargs='+',
+            help='Path to directory containing dialogue transcripts')
+    parser.add_argument('--worker-ids', nargs='+',
+            help='Path to json file containing chat_id to worker_id mappings')
+    parser.add_argument('--summary', default=False, action='store_true',
+            help='Summarize human ratings')
+    parser.add_argument('--hist', default=False, action='store_true',
+            help='Plot histgram of ratings')
+    parser.add_argument('--html-visualize', action='store_true',
+            help='Output html files')
     parser.add_argument('--outdir', default='.', help='Output dir')
-    parser.add_argument('--stats', default='stats.json', help='Path to stats file')
-    parser.add_argument('--partner', default=False, action='store_true', help='Whether this is from partner survey')
+    parser.add_argument('--stats', default='stats.json',
+            help='Path to stats file')
+    parser.add_argument('--partner', default=False, action='store_true',
+            help='Whether this is from partner survey')
     HTMLVisualizer.add_html_visualizer_arguments(parser)
     args = parser.parse_args()
 
-    visualizer = Visualizer(args.dialogue_transcripts, args.survey_transcripts, args.worker_ids)
+    visualizer = Visualizer(args.dialogue_transcripts,
+            args.survey_transcripts, args.worker_ids)
 
     visualizer.compute_effectiveness()
 
@@ -35,4 +44,6 @@ if __name__ == '__main__':
         visualizer.worker_stats()
 
     if args.html_output:
-        visualizer.html_visualize(args.viewer_mode, args.html_output, css_file=args.css_file, img_path=args.img_path, worker_ids=visualizer.worker_ids)
+        visualizer.html_visualize(args.viewer_mode, args.html_output,
+            css_file=args.css_file, img_path=args.img_path,
+            worker_ids=visualizer.worker_ids)
