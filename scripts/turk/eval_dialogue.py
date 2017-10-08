6034bb1bef8de6eff7bd36976c41c04970f1aaba
hehe <hhe.xiy@gmail.com>
2017-10-08 08:30:04 -0700

    merge

--- scripts/turk/eval_dialogue.py
+++ scripts/turk/eval_dialogue.py
@@ -54,6 +54,7 @@ if __name__ == '__main__':
             max_assignments=args.num_assignments_per_hit,
             db_path=args.hit_db,
             lifetime=lifetime,
+            qualifications=None,
             )
 
     if args.check_workers:
