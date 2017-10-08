1dcfaae2c8a50e90636056babef90be04cffaede
Derek Chen <derekchen14@gmail.com>
2017-10-08 11:25:24 -0700

    Update the shell scripts to be able to run the visuaizer

--- fb-negotiation/run.sh
+++ fb-negotiation/run.sh
@@ -6,7 +6,13 @@
 #     --agents rulebased cmd \
 #     --verbose --random-seed 8
 
-PYTHONPATH=. python web/chat_app.py --port 1414 \
-    --schema-path data/bookhatball-schema.json \
-    --config web/app_params.json --scenarios-path data/test-scenarios.json \
-    --output web/output
+# PYTHONPATH=. python web/chat_app.py --port 1414 \
+#     --schema-path data/bookhatball-schema.json \
+#     --config web/app_params.json --scenarios-path data/test-scenarios.json \
+#     --output web/output
+
+PYTHONPATH=. python ../scripts/visualize_transcripts.py \
+    --dialogue-transcripts web/output/transcripts/transcripts.json \
+    --survey-transcripts web/output/transcripts/surveys.json \
+    --html-output web/templates/visualize.html --img-path images \
+    --css-file ../chat_viewer/css/my.css
