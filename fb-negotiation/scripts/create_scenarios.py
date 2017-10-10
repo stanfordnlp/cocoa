339dd83e97ea4d86be21867732e2d02f05146479
hehe <hhe.xiy@gmail.com>
2017-10-10 16:29:11 -0700

    check singletons in scenarios

--- fb-negotiation/scripts/create_scenarios.py
+++ fb-negotiation/scripts/create_scenarios.py
@@ -1,4 +1,5 @@
 import json
+import argparse
 import copy
 from cocoa.core.scenario_db import ScenarioDB
 from cocoa.core.schema import Schema
@@ -6,13 +7,16 @@ from cocoa.core.util import generate_uuid, write_json, read_json
 from core.kb import KB
 from core.scenario import Scenario
 
-root_path = "data/"
-scenario_list = []
+parser = argparse.ArgumentParser()
+parser.add_argument('--schema-path')
+parser.add_argument('--scenario-ints-file', help='Path to the file containing 6 integers per line that describes the scenario')
+parser.add_argument('--output', help='Path to the output JSON scenario file')
+args = parser.parse_args()
 
-schema = Schema(root_path + "bookhatball-schema.json")
+schema = Schema(args.schema_path)
 
 scenarios = []
-with open(root_path + "selfplay.txt") as fin:
+with open(args.scenario_ints_file) as fin:
     kbs = []
     names = ['book', 'hat', 'ball']
     for line in fin:
@@ -20,9 +24,13 @@ with open(root_path + "selfplay.txt") as fin:
         kb = KB.from_ints(schema.attributes, names, ints)
         kbs.append(kb)
         if len(kbs) == 2:
+            if kbs[0].item_counts != kbs[1].item_counts:
+                del kbs[0]
+                continue
+            assert kbs[0].item_counts == kbs[1].item_counts
             scenario = Scenario(generate_uuid("FB"), schema.attributes, kbs)
             scenarios.append(scenario)
             kbs = []
 
 scenario_db = ScenarioDB(scenarios)
-write_json(scenario_db.to_dict(), root_path + "test-scenarios.json")
+write_json(scenario_db.to_dict(), args.output)
