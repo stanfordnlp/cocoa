3263ab0adb9cd8a04b64341ef4d345ae60d8d6b4
hehe <hhe.xiy@gmail.com>
2017-11-04 21:54:27 -0700

    add fb neural bot

--- fb-negotiation/systems/__init__.py
+++ fb-negotiation/systems/__init__.py
@@ -1,9 +1,11 @@
 from core.lexicon import Lexicon
 from rulebased_system import RulebasedSystem
 from cmd_system import CmdSystem
+from neural_system import NeuralSystem, add_neural_system_arguments
 
 def add_system_arguments(parser):
     parser.add_argument('--mappings', default='.', help='Directory to save mappings/vocab')
+    add_neural_system_arguments(parser)
 
 def get_system(name, args, schema=None, timed=False):
     lexicon = Lexicon(schema.values['item'])
@@ -11,5 +13,7 @@ def get_system(name, args, schema=None, timed=False):
         return RulebasedSystem(lexicon, timed)
     elif name == 'cmd':
         return CmdSystem()
+    elif name == 'neural':
+        return NeuralSystem(args.model_file, args.temperature, timed)
     else:
         raise ValueError('Unknown system %s' % name)
