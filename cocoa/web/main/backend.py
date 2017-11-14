b7b141790cf766968dea98d027044c6bd18b68a8
hehe <hhe.xiy@gmail.com>
2017-11-14 10:23:34 -0800

    refactor: put get_backend in cocoa

--- cocoa/web/main/backend.py
+++ cocoa/web/main/backend.py
@@ -880,3 +880,25 @@ class Backend(object):
         except sqlite3.IntegrityError:
             print("WARNING: Rolled back transaction")
             return None
+
+#######################################
+from flask import g
+from flask import current_app as app
+from utils import Messages
+
+def get_backend():
+    backend = getattr(g, '_backend', None)
+    if backend is None:
+        g._backend = Backend(app.config["user_params"],
+                         app.config["schema"],
+                         app.config["scenario_db"],
+                         app.config["systems"],
+                         app.config["sessions"],
+                         app.config["controller_map"],
+                         app.config["num_chats_per_scenario"],
+                         Messages,
+                         active_system=app.config.get('active_system'),
+                         active_scenario=app.config.get('active_scenario'),
+                         )
+        backend = g._backend
+    return backend
