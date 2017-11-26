ff6d46737d4ab0485c58c692b77ea60171bdaf69
hehe <hhe.xiy@gmail.com>
2017-11-26 10:37:48 -0800

    tokenizer

--- cocoa/core/tokenizer.py
+++ cocoa/core/tokenizer.py
@@ -1,6 +1,15 @@
+import nltk
+nltk.download('punkt')
+from nltk.tokenize import word_tokenize
 from nltk.tokenize.moses import MosesDetokenizer
 
 detokenizer = MosesDetokenizer()
 
 def detokenize(tokens):
     return detokenizer.detokenize(tokens, return_str=True)
+
+def tokenize(utterance, lowercase=True):
+    if lowercase:
+        utterance = utterance.lower()
+    tokens = word_tokenize(utterance)
+    return tokens
