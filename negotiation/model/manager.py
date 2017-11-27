97c6f2f6bce576c4edef836ee509fd5d15f658e9
hehe <hhe.xiy@gmail.com>
2017-11-26 19:22:11 -0800

    don't offer given unknown partner act

--- negotiation/model/manager.py
+++ negotiation/model/manager.py
@@ -8,9 +8,14 @@ class Manager(BaseManager):
             masked_actions.append('inquiry')
             if state.curr_price is None:
                 actions = ['init-price']
-        if state.partner_price is None or state.curr_price is None or 'offer' in state.done:
+        if state.partner_price is None or state.curr_price is None:
             masked_actions.append('offer')
         actions = [a for a in actions if not a in masked_actions]
         return actions
 
+    def choose_action(self, state, context=None):
+        action = super(Manager, self).choose_action(state, context)
+        if action == 'offer' and self.partner_act == 'unknown':
+            return 'agree'
+        return action
 
