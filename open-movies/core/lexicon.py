ccd636d728af325ffbf5f8cc525a063406c9da8e
hehe <hhe.xiy@gmail.com>
2018-01-02 23:40:12 -0800

    use spacy to detect NP

--- open-movies/core/lexicon.py
+++ open-movies/core/lexicon.py
@@ -1,9 +1,11 @@
 import re
 import time
+import spacy
 from fuzzywuzzy import fuzz
 import numpy as np
 from nltk import ngrams, pos_tag, RegexpParser, Tree
 from datasketch import MinHash, MinHashLSH
+from itertools import ifilter
 
 from cocoa.core.util import generate_uuid, write_pickle, read_pickle
 from cocoa.core.entity import CanonicalEntity, Entity
@@ -14,11 +16,11 @@ def add_lexicon_arguments(parser):
 class Lexicon(object):
     # Titles that are confusing...
     stopwords = ['yes', 'hello', 'i', 'good morning', 'love', 'men', 'no', 'the day', 'i wish', 'lol', 'the first time', 'the dog', 'the lady']
+    nlp = spacy.load('en')
 
     def __init__(self, entities=None, threshold=0.8, lsh=None):
         self.entities = entities
         self.lsh = self.build_lsh(threshold) if not lsh else lsh
-        self.chunker = self.build_np_chunker()
 
     def save_pickle(self, path):
         print 'Dump lexicon to {}'.format(path)
@@ -28,13 +30,6 @@ class Lexicon(object):
     def from_pickle(cls, path):
         data = read_pickle(path)
         return cls(**data)
-
-    @classmethod
-    def build_np_chunker(cls):
-        grammar = r"""
-          NP: {<DT|PP\$>?<JJ.*>*<NN.*>+}
-        """
-        return RegexpParser(grammar)
 
     @classmethod
     def from_csv(cls, path, threshold):
@@ -57,7 +52,7 @@ class Lexicon(object):
     def minhash(cls, s):
         m = MinHash(num_perm=128)
         s = re.sub(r'[^a-z0-9]', '', s.lower())
-        for ss in ngrams(s, 4):
+        for ss in ngrams('^^^'+s, 4):
             m.update(''.join(ss))
         return m
 
@@ -78,19 +73,7 @@ class Lexicon(object):
         entity_ids = self.lsh.query(q)
         return [self.entities[id_] for id_ in entity_ids]
 
-    def chunk(self, tokens):
-        tagged_tokens = pos_tag(tokens)
-        tree = self.chunker.parse(tagged_tokens)
-        chunks = []
-        for node in tree:
-            if isinstance(node, Tree) and node.label() == 'NP':
-                tags = [x[1] for x in node.leaves()]
-                type_ = 'entity' if 'NNP' in tags else 'NP'
-                yield ([x[0] for x in node.leaves()], type_)
-            else:
-                yield node[0]
-
-    def link_entity(self, tokens):
+    def link_entity(self, text):
         """Link tokens to entities.
 
         Example:
@@ -98,28 +81,49 @@ class Lexicon(object):
             ['i', 'work', 'at', ('apple', ('apple','company'))]
 
         """
-        entity_tokens = []
-        for chunk in self.chunk(tokens):
-            if isinstance(chunk, tuple):
-                chunk, type_ = chunk
-                s = ' '.join(chunk)
-                entities = self.query(s, k=1)
-                if entities:
-                    sorted_entities = sorted(entities, key=lambda x: fuzz.ratio(s.lower(), x.value.lower()), reverse=True)
-                    for entity in sorted_entities:
-                        if not entity.value.lower() in self.stopwords:
-                            break
-                    if (not entity.value.lower() in self.stopwords) and \
-                            fuzz.ratio(s.lower(), entity.value.lower()) > 70:
-                        entity_tokens.append(Entity(surface=s, canonical=entity))
-                    else:
-                        entity_tokens.extend(chunk)
-                elif type_ == 'entity':
-                    entity_tokens.append(Entity.from_elements(surface=s, value=s, type='unknown'))
-                else:
-                    entity_tokens.extend(chunk)
-            else:
-                entity_tokens.append(chunk)
+        doc = self.nlp(unicode(text))
+        entities = []
+        for np in doc.noun_chunks:
+            s = np.text
+            print s
+            candidates = self.query(s, k=1)
+            if candidates:
+                sorted_candidates = sorted(candidates, key=lambda x: fuzz.ratio(s.lower(), x.value.lower()), reverse=True)
+                for candidate in ifilter(lambda e: e.value.lower() not in self.stopwords, sorted_candidates):
+                    if fuzz.ratio(s.lower(), candidate.value.lower()) > 70:
+                        entity = Entity(surface=s, canonical=candidate)
+                        entities.append((entity, np.start, np.end))
+                        # Take the best matched candidate
+                        break
+
+        def overlap(e, entities):
+            for entity in entities:
+                if not (e.start >= entity[2] or e.end < entity[1]):
+                    return True
+            return False
+
+        for ent in doc.ents:
+            if ent.label_ == 'PERSON':
+                entity = Entity.from_elements(surface=ent.text, value=ent.text, type='person')
+                entities.append((entity, ent.start, ent.end))
+            elif ent.label_ == 'WORK_OF_ART' and not overlap(ent, entities):
+                entity = Entity.from_elements(surface=ent.text, value=ent.text, type='title')
+                entities.append((entity, ent.start, ent.end))
+
+        tokens = [tok.text for tok in doc]
+        if not entities:
+            entity_tokens = tokens
+        else:
+            last = 0
+            entity_tokens = []
+            entities = sorted(entities, key=lambda x: x[1])
+            for entity in entities:
+                entity, start, end = entity
+                entity_tokens.extend(tokens[last:start])
+                entity_tokens.append(entity)
+                last = end
+            if last < len(tokens):
+                entity_tokens.extend(tokens[last:])
         return entity_tokens
 
 if __name__ == '__main__':
@@ -144,5 +148,12 @@ if __name__ == '__main__':
         lexicon.save_pickle(args.output)
 
     from core.tokenizer import tokenize
-    tokens = tokenize('just saw dunkirk', lowercase=True)
-    print lexicon.link_entity(tokens)
+    sents = [
+            '''I dont either. I also like the monty python ones''',
+            '''I love that one. I also really like The Last Castle. I guess we both like prison movies!''',
+            '''I haven't seen much new, but am going on Sunday to see Bad Mom's Christmas. The first one was funny. Hoping the sequel is good as well!''',
+            '''No, the last good movie in theaters was Get Out by Jordan Peele.''',
+            '''Nice. I love The Room, but I can't stand the one that James Franco just did.''',
+            ]
+    for s in sents:
+        print lexicon.link_entity(s)
