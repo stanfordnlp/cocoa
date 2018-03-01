225014bcbd2e9d4b888a416b83b8bbaf6b06c5a6
hehe <hhe.xiy@gmail.com>
2018-02-28 17:17:09 -0800

    analyze scripts

---
+++ mutualfriends/scripts/analyze.py
@@ -0,0 +1,44 @@
+import argparse
+
+from cocoa.core.schema import Schema
+from cocoa.core.dataset import read_examples
+from cocoa.model.dialogue_parser import parse_example
+from cocoa.analysis.analyzer import Analyzer
+
+from core.scenario import Scenario
+from core.lexicon import Lexicon, add_lexicon_arguments
+from model.generator import Templates
+from model.manager import Manager
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--transcripts', nargs='*', help='JSON transcripts to extract templates')
+    parser.add_argument('--max-examples', default=-1, type=int)
+    parser.add_argument('--templates', help='Path to load templates')
+    parser.add_argument('--policy', help='Path to load model')
+    parser.add_argument('--schema-path', help='Path to schema')
+    parser.add_argument('--agent', help='Only consider examples with the given type of agent')
+    add_lexicon_arguments(parser)
+    args = parser.parse_args()
+
+    schema = Schema(args.schema_path)
+    lexicon = Lexicon(schema, False, stop_words=args.stop_words, lexicon_path=args.lexicon)
+    #templates = Templates.from_pickle(args.templates)
+    templates = Templates()
+    manager = Manager.from_pickle(args.policy)
+    analyzer = Analyzer(lexicon)
+
+    examples = read_examples(args.transcripts, args.max_examples, Scenario)
+    agent = args.agent
+    if agent is not None:
+        examples = [e for e in examples if agent in e.agents.values()]
+    analyzer.example_stats(examples, agent=agent)
+    #import sys; sys.exit()
+
+    parsed_dialogues = []
+    for example in examples:
+        utterances = parse_example(example, lexicon, templates)
+        parsed_dialogues.append(utterances)
+
+    analyzer.parser_stats(parsed_dialogues, agent=agent)
+    #analyzer.manager_stats(manager)
