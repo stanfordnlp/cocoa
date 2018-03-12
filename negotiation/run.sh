fff77da4069b45a9d215e3a74d1f14c8ecc3712f
Derek Chen <derekchen14@gmail.com>
2018-03-12 11:35:08 -0700

    first draft at new makefile for neg, rename train to pt_main

--- negotiation/run.sh
+++ negotiation/run.sh
@@ -7,13 +7,25 @@
 #     --price-tracker-model price-tracker.pkl \
 #     --random-seed 8
 
-PYTHONPATH=. python web/chat_app.py --port 5000 \
-    --schema-path data/craigslist-schema.json --templates templates.pkl \
-    --config web/app_params.json --scenarios-path data/sample-scenarios.json \
-    --output web/output --price-tracker price-tracker.pkl\
+# PYTHONPATH=. python web/chat_app.py --port 5000 \
+#     --schema-path data/craigslist-schema.json --templates templates.pkl \
+#     --config web/app_params.json --scenarios-path data/sample-scenarios.json \
+#     --output web/output --price-tracker price-tracker.pkl\
 
 # PYTHONPATH=. python ../scripts/visualize_transcripts.py \
 #     --dialogue-transcripts web/output/transcripts/transcripts.json \
 #     --survey-transcripts web/output/transcripts/surveys.json \
 #     --html-output web/output/transcripts/transcripts.html \
 #     --img-path images --css-file ../chat_viewer/css/my.css
+
+PYTHONPATH=. python pt_main.py --schema-path data/schema.json \
+      --train-examples-paths data/train.json --test-examples-paths data/dev.json \
+      --train-max-examples 250 --test-max-examples 250 --mappings data/mappings \
+      --stats-file data/stats.train --pretrained-wordvec data/mappings/glove.pt \
+      --word-vec-size 300 --verbose --ignore-cache
+
+# PYTHONPATH=. python ../scripts/split_dataset.py --example-paths scr/web_output/combined/transcripts/transcripts.json --train-frac 0.8 --test-frac 0.1 --dev-frac 0.1 --output-path data/
+
+# PYTHONPATH=. python ../scripts/split_dataset.py --example-paths /juicy/scr61/scr/nlp/hehe/cocoa/open-movie/web_output/combined/transcripts/transcripts.json --train-frac 0.8 --test-frac 0.1 --dev-frac 0.1 --output-path data/
+
+# PYTHONPATH=. python ../cocoa/neural/embeddings_to_torch.py --emb-file /scr/derekchen14/datasets/glove/glove.840B.300d.txt --vocab-file $(mappings)/vocab.pkl --output-file data/glove
