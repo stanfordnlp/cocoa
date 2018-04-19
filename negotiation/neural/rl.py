30347f73755044859b889262ea9e1150f83afccd
Derek Chen <derekchen14@gmail.com>
2018-04-18 18:11:55 -0700

    update rl to include reward calculation

--- negotiation/neural/rl.py
+++ negotiation/neural/rl.py
@@ -5,7 +5,6 @@ import numpy as np
 
 from core.controller import Controller
 
-
 def add_rl_arguments(parser):
     group = parser.add_argument_group('REINFORCE')
     parser.add_argument('--max-turns', default=100, type=int, help='Maximum number of turns')
@@ -23,7 +22,7 @@ class Reinforce(object):
         self.agents = agents
         self.scenarios = scenarios
 
-    def _get_controller(self):
+    def _set_controller(self):
         scenario = random.choice(self.scenarios)
         # Randomize because kb[0] is always seller
         if random.random() < 0.5:
@@ -32,26 +31,40 @@ class Reinforce(object):
             kbs = (scenario.kbs[1], scenario.kbs[0])
         sessions = [self.agents[0].new_session(0, kbs[0], rl=True),
                     self.agents[1].new_session(1, kbs[1], rl=False)]
-        controller = Controller(scenario, sessions)
-        return controller
+        self.controller = Controller(scenario, sessions)
 
     def get_reward(self, ex):
-        outcome = get_outcome()
-        reward = outcome['reward']
-        offer = outcome['offer']
         # reward = reward if agree else 0
         # self.all_rewards.append(reward)
         # standardize the reward
         # r = (reward - np.mean(self.all_rewards)) / max(1e-4, np.std(self.all_rewards))
+        outcome = self.controller.get_outcome()
+        reward = outcome['reward']
+        offer = outcome['offer']
+        if not self.agent.has_deal(outcome):
+            return reward
 
+        margins = {'seller': 0, 'buyer': 0}
+        targets = {}
+        # for agent in agents?
+        for role in ('seller', 'buyer'):
+            # kb comes from scenario?
+            targets[role] = self.kb_by_role[role].facts["personal"]["Target"]
+        midpoint = (targets['seller'] + targets['buyer']) / 2.
+        price = self.outcome['offer']['price']
 
+        norm_factor = abs(midpoint - targets['seller'])
+        margins['seller'] = (price - midpoint) / norm_factor
+        # Zero sum
+        margins['buyer'] = -1. * margins['seller']
+        return margins
 
     def learn(self, opt):
         for i in xrange(opt.num_dialogues):
-            controller = self._get_controller()
-            ex = controller.simulate(max_turns, verbose=args.verbose)
+            self._set_controller()
+            ex = self.controller.simulate(max_turns, verbose=args.verbose)
             rewards = self.get_reward(ex)
-            for session in controller.sessions:
+            for session in self.controller.sessions:
                 if hasattr(session, 'trainable') and session.trainable:
                     session.update(reward)
             # TODO: logging
