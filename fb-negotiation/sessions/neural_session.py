fa3f11af04a85261e856402ae5ff178d0433b69f
Derek Chen <derekchen14@gmail.com>
2018-05-06 16:53:56 -0700

    Create new pytorch sessions and system for fb-neg

--- fb-negotiation/sessions/neural_session.py
+++ fb-negotiation/sessions/neural_session.py
@@ -77,3 +77,99 @@ class NeuralSession(Session):
             return self.select()
         # Omit the last <eos> symbol
         return self.message(' '.join(tokens[:-1]))
+
+class PytorchNeuralSession(NeuralSession):
+    def __init__(self, agent, kb, env):
+        super(PytorchNeuralSession, self).__init__(agent, kb, env)
+        self.vocab = env.vocab
+        self.gt_prefix = env.gt_prefix
+
+        self.dec_state = None
+        self.stateful = self.env.model.stateful
+
+        self.new_turn = False
+        self.end_turn = False
+
+    def get_decoder_inputs(self):
+        # Don't include EOS
+        utterance = self.dialogue._insert_markers(self.agent, [], True)[:-1]
+        inputs = self.env.textint_map.text_to_int(utterance, 'decoding')
+        inputs = np.array(inputs, dtype=np.int32).reshape([1, -1])
+        return inputs
+
+    def _create_batch(self):
+        num_context = Dialogue.num_context
+
+        # All turns up to now
+        self.convert_to_int()
+        encoder_turns = self.batcher._get_turn_batch_at([self.dialogue], Dialogue.ENC, None)
+
+        encoder_inputs = self.batcher.get_encoder_inputs(encoder_turns)
+        encoder_context = self.batcher.get_encoder_context(encoder_turns, num_context)
+        encoder_args = {
+                        'inputs': encoder_inputs,
+                        'context': encoder_context
+                    }
+        decoder_args = {
+                        'inputs': self.get_decoder_inputs(),
+                        'context': self.kb_context_batch,
+                        'targets': np.copy(encoder_turns[0]),
+                    }
+
+        context_data = {
+                'agents': [self.agent],
+                'kbs': [self.kb],
+                }
+
+        return Batch(encoder_args, decoder_args, context_data,
+                self.vocab, sort_by_length=False, num_context=num_context, cuda=self.cuda)
+
+    def generate(self):
+        if len(self.dialogue.agents) == 0:
+            self.dialogue._add_utterance(1 - self.agent, [])
+        batch = self._create_batch()
+
+        enc_state = self.dec_state.hidden if self.dec_state is not None else None
+        output_data = self.generator.generate_batch(batch, gt_prefix=self.gt_prefix, enc_state=enc_state)
+
+        if self.stateful:
+            # TODO: only works for Sampler for now. cannot do beam search.
+            self.dec_state = output_data['dec_states']
+        else:
+            self.dec_state = None
+
+        entity_tokens = self._output_to_tokens(output_data)
+
+        #if not self._is_valid(entity_tokens):
+        #    return None
+        return entity_tokens
+
+    def _is_valid(self, tokens):
+        if not tokens:
+            return False
+        if Vocabulary.UNK in tokens:
+            return False
+        return True
+
+    def _output_to_tokens(self, data):
+        predictions = data["predictions"][0][0]
+        tokens = self.builder.build_target_tokens(predictions, self.kb)
+        return tokens
+
+    # To support REINFORCE
+    # TODO: this should be in NeuralSession
+    def iter_batches(self):
+        """Compute the logprob of each generated utterance.
+        """
+        self.convert_to_int()
+        batches = self.batcher.create_batch([self.dialogue])
+        yield len(batches)
+        for batch in batches:
+            # TODO: this should be in batcher
+            batch = Batch(batch['encoder_args'],
+                          batch['decoder_args'],
+                          batch['context_data'],
+                          self.env.vocab,
+                          num_context=Dialogue.num_context, cuda=self.env.cuda)
+            yield batch
+
