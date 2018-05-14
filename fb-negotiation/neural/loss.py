39e2ee510ad56b1d7454af3cf92153aabfad2a59
Derek Chen <derekchen14@gmail.com>
2018-05-13 20:22:55 -0700

    Update loss to size average

--- fb-negotiation/neural/loss.py
+++ fb-negotiation/neural/loss.py
@@ -14,7 +14,7 @@ class FBnegLossCompute(SimpleLossCompute):
     def __init__(self, generator, vocab):
         super(FBnegLossCompute, self).__init__(generator, vocab)
         self.selector = nn.LogSoftmax()
-        self.select_criterion = nn.NLLLoss()
+        self.select_criterion = nn.NLLLoss(size_average=False)
         self.vocab = vocab
 
     def compute_loss(self, targets, selections, output):
@@ -23,23 +23,12 @@ class FBnegLossCompute(SimpleLossCompute):
         # output_decoder (13, 4, 256), bottled = (52, 256)
         scores = self.generator(self._bottle(output["decoder"]))
         # scores = (52 x 1246), (seq_len x vocab_size) since 1246 is vocab size
-        gtruth = targets.contiguous().view(-1)
+        utterance_truth = targets.contiguous().view(-1)
         # targets (13 x 4), so ground_truth = 52
-        loss = self.criterion(scores, gtruth)
-        loss_data = loss.data.clone()
+        utterance_loss = self.criterion(scores, utterance_truth)
+        loss_data = utterance_loss.data.clone()
         stats = self._stats(loss_data, scores.data, targets.view(-1).data)
-        '''
-        if random.random() < 0.003:
-            seq_len, batch_size, hidden_dim = output["decoder"].shape
-            print "loss:", loss.data.cpu().numpy()[0]
-            # choose the 4th example after transposing 
-            top = targets.transpose(0,1)[3].data.cpu().numpy()
-            print " ".join([str(self.vocab.to_word(x)) for x in top if x != 1244])
-            bottom = torch.max(scores.view(seq_len, batch_size, -1), dim=2)[1].data.cpu().numpy()
-            # choose the 4th example
-            print " ".join([str(self.vocab.to_word(x)) for x in bottom[:,3] if x != 1244])
-            pdb.set_trace()
-        '''
+        
         # selector: GRU outputs to kb vocab_size scores/logprobs
         # output.selector (6, batch_size, kb_vocab_size) = (6, 16, 28)
         select_scores = self.selector(self._bottle(output["selector"]))
@@ -51,11 +40,19 @@ class FBnegLossCompute(SimpleLossCompute):
         select_data = select_loss.data.clone()
         select_stats = self._stats(select_data, select_scores.data,
                             select_truth.data)
+        '''        
+        if random.random() < 0.01:
+            print "utterance_loss:", utterance_loss.data.cpu().numpy()[0]
+            print "select_loss:", select_loss.data.cpu().numpy()[0]
+            pdb.set_trace()
 
-        # if random.random() < 0.01:
-        #     print "utterance_loss:", loss.data.cpu().numpy()[0]
-        #     print "select_loss:", select_loss.data.cpu().numpy()[0]
-        total_loss = loss + select_loss
+        For both losses, size_average is set to False, which means we sum
+        the batch loss, rather than averaging it.  This is because the input
+        of the loss function is resized from (seq_len, batch_size, vocab_size)
+        into ((seq_len * batch_size), vocab_size), so averaging would divide
+        by a factor of sequence length (which varies for each batch) and is
+        thus uncertain.  Therefore, we later divide by batch size to normalize.'''
+        loss = utterance_loss + select_loss
         stats.update(select_stats)
 
-        return total_loss, stats
+        return loss, stats
