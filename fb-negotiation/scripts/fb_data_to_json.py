ed912b51c160d18716c54fc9ad0909608c53baa0
hehe <hhe.xiy@gmail.com>
2018-05-17 08:45:52 -0700

    remove _v2 in filename

--- fb-negotiation/scripts/fb_data_to_json.py
+++ fb-negotiation/scripts/fb_data_to_json.py
@@ -1,165 +1,228 @@
+import random
 import json
-import argparse
-import copy
+import string
+import sys
+import pdb
 import time as tm
 
-def fill_scenario(knowledge_bases, stamp):
-  attr_item = { 'entity': True, 'unique': False, 'value_type': 'item',
-                'name': 'Name', 'multivalued': False }
-  attr_integer = { 'entity': True, 'unique': False, 'value_type': 'integer',
-                'name': 'Name', 'multivalued': False }
-  scenario_dict = {
-    'attributes': [attr_item, attr_integer, attr_integer],
-    'uuid': 'scenario_uuid_{}'.format(stamp),
-    'kbs': knowledge_bases
-  }
-  return scenario_dict
+def generate_uuid(prefix):
+  return prefix + '_' + ''.join([random.choice(string.digits + string.letters) for _ in range(16)])
 
-def fit_into_transcripts(data, counter):
-  stamp = str(counter).zfill(3)
-  scenario_dict = fill_scenario( data['kbs'], stamp )
-  transcript = {
-    'uuid': 'uuid_{}'.format(stamp),
-    'scenario': scenario_dict,
-    'agents_info': {'config': [8, 5, 10]},
-    'scenario_uuid': 'scenario_uuid_{}'.format(stamp),
-    'agents': {0: 'human', 1: 'human'},
-    'outcome': data['outcome'],
-    'events': data['events']
-  }
-  return transcript
-
-def make_kb(tokens):
-  scores = [int(t) for t in tokens if not t.startswith("<")]
-  kb = [
-    {'Count': scores[0], 'Name': 'book', 'Value': scores[1]},
-    {'Count': scores[2], 'Name': 'hat', 'Value': scores[3]},
-    {'Count': scores[4], 'Name': 'ball', 'Value': scores[5]}
-  ]
-  return kb
-
-def break_into_turns(tokens):
+def populate_events(dialogue, outcome):
+  events = []
+  timestep = 0
   turns = []
-  event = []
-  for token in tokens:
-    if token in ['<dialogue>', '</dialogue>']:
-      continue
-    if token == '<eos>':
-      turns.append(event)
-      event = []
-    else:
-      event.append(token)
-  return turns
+  token_turns = [0]
 
-def calculate_rewards(counts, scenario):
-  reward = 0
-  for idx, item in enumerate(['book', 'hat', 'ball']):
-    value = scenario[idx]['Value']
-    count = counts[item]
-    reward += value * count
-  return reward
-
-def calculate_splits(tokens):
-  splits = [int(t.split("=")[1]) for t in tokens if not t.startswith("<")]
-  items = { 0: {'book': splits[0], 'hat': splits[1], 'ball': splits[2]},
-            1: {'book': splits[3], 'hat': splits[4], 'ball': splits[5]}   }
-  return items
-
-def find_results(tokens, scenario):
-  deal_validity = True
-  agreement = True
-  if tokens[1].startswith('item'):
-    items = calculate_splits(tokens)
-    reward_0 = calculate_rewards(items[0], scenario[0])
-    reward_1 = calculate_rewards(items[1], scenario[1])
+  if dialogue[0] == "THEM:":
+    agents = {"THEM:": 0, "YOU:": 1}
+  elif dialogue[0] == "YOU:":
+    agents = {"THEM:": 1, "YOU:": 0}
   else:
-    items = {0:{'book':0, 'hat':0, 'ball':0}, 1:{'book':0, 'hat':0, 'ball':0}}
-    reward_0, reward_1 = (0,0)
-    if tokens[1] == '<disagree>':
-      deal_validity = False
-    elif tokens[1] in ['<no_agreement>', '<disconnect>']:
-      agreement = False
-    else:
-      print "Something went wrong: {}".format(tokens)
+    print "ERROR:", dialogue
+    sys.exit()
 
-  outcome_dict = {
-    'valid_deal': deal_validity,
-    'item_split': items,
-    'reward': {0: reward_0, 1: reward_1},
-    'agreed': agreement
-  }
-  return outcome_dict
+  for token in dialogue:
+    if token in ["THEM:", "YOU:"] and len(token_turns) > 1:
+      # we are in a new turn, close the previous turn
+      turns.append(token_turns)
+      # start a new turn
+      token_turns = [agents[token]]
+    elif token not in ["<eos>", "THEM:", "YOU:"]:
+      token_turns.append(token)
+  # grab the last selection token
+  if len(token_turns) > 1:
+    turns.append(token_turns)
 
-def populate_event(agent_id, action, data):
-  timestamp = tm.time()
+  for turn in turns:
+    if turn[1] == "<selection>":
+      event = create_one_event(turn[0], timestep, turn[1], "select")
+    else:  # message actions
+      event = create_one_event(turn[0], timestep, turn[1:])
+    events.append(event)
+    timestep += 1
+
+  return events
+
+def create_one_event(agent, timestep, data, action="message"):
+  text = " ".join(data) if action == "message" else data
   event = {
-    'start_time': timestamp,
-    'agent': agent_id,
-    'template': None,
-    'time': timestamp,
-    'action': action,
-    'data': data
+    "start_time": tm.time(),
+    "agent": agent,
+    "template": None,
+    "time": timestep,
+    "action": action,
+    "data": text
   }
   return event
 
-def parse_events(dialogue, outcome):
-  turns = break_into_turns(dialogue)
-  event_list = []
-  for action in turns:
-    agent_id = 0 if action.pop(0) == 'YOU:' else 1 # agent == THEM:
-    msg_event = populate_event(agent_id, 'message', " ".join(action))
-    event_list.append(msg_event)
-  if outcome['valid_deal'] and outcome['agreed']:
-    items = outcome['item_split']
+def create_scenario(data, sid):
+
+  attr = [{"entity": True, "unique": False, "value_type": "item",
+              "name": "Name", "multivalued": False },
+          {"entity": True, "unique": False, "value_type": "integer",
+              "name": "Name", "multivalued": False },
+          {"entity": True, "unique": False, "value_type": "integer",
+              "name": "Name", "multivalued": False }
+          ]
+  kbs = create_knowledge_bases(data)
+  return {"attributes": attr, "uuid": sid, "kbs": kbs}
+
+def create_knowledge_bases(data):
+  my_input = data['input']
+  part_input = data['part_input']
+
+  my_kb = [
+    {"Count": int(my_input[0]), "Name": "book", "Value": int(my_input[1])},
+    {"Count": int(my_input[2]), "Name": "hat", "Value": int(my_input[3])},
+    {"Count": int(my_input[4]), "Name": "ball", "Value": int(my_input[5])}
+  ]
+  part_kb = [
+    {"Count": int(part_input[0]), "Name": "book", "Value": int(part_input[1])},
+    {"Count": int(part_input[2]), "Name": "hat", "Value": int(part_input[3])},
+    {"Count": int(part_input[4]), "Name": "ball", "Value": int(part_input[5])}
+  ]
+
+  return [my_kb, part_kb]
+
+def read_file(in_filename):
+  in_lines = []
+  with open(in_filename, 'r') as infile:
+    for line in infile:
+      in_lines.append(line.strip())
+  return in_lines
+
+def store_lines(outfile, cleaned):
+  with open(outfile, 'w') as file:
+    json.dump(cleaned, file)
+  print("Completed saving {0} lines into {1}".format(len(cleaned), outfile) )
+
+def parse_lines(raw):
+  parsed = []
+  for line in raw:
+    result = parse_one_line(line.split())
+    parsed.append(result)
+  return parsed
+
+def parse_one_line(tokens):
+  result = {
+    "input": get_tag(tokens, 'input'),
+    "dialogue": get_tag(tokens, 'dialogue'),
+    "output": get_tag(tokens, 'output'),
+    "part_input": get_tag(tokens, 'partner_input'),
+  }
+  return result
+
+def get_tag(tokens, tag):
+  # the index right after the start tag
+  start = tokens.index('<' + tag + '>') + 1
+  # the index of the ending tag
+  stop = tokens.index('</' + tag + '>')
+  return tokens[start:stop]
+
+def create_outcome(data):
+  out = data['output']
+
+  if out[0] in ["<disagree>", "<disconnect>", "<no_agreement>"]:
+    outcome = {
+      "valid_deal": False,
+      "item_split": [
+        {"book": 0, "hat": 0, "ball": 0},
+        {"book": 0, "hat": 0, "ball": 0}
+      ],
+      "reward": 0,
+      "agreed": False
+    }
+    return outcome
+
+  my_book = int(out[0].split("=")[1])
+  my_hat = int(out[1].split("=")[1])
+  my_ball = int(out[2].split("=")[1])
+  part_book = int(out[3].split("=")[1])
+  part_hat = int(out[4].split("=")[1])
+  part_ball = int(out[5].split("=")[1])
+
+  outcome = {
+    "valid_deal": determine_validity(data),
+    "item_split": [
+      {"book": my_book, "hat": my_hat, "ball": my_ball},
+      {"book": part_book, "hat": part_hat, "ball": part_ball}
+    ],
+    "reward": calculate_reward(data),
+    "agreed": data['dialogue'][-1] == "<selection>"
+  }
+
+  return outcome
+
+def calculate_reward(data):
+  out = data['output']
+  my_input = data['input']
+  part_input = data['part_input']
+
+  my_book = int(out[0].split("=")[1])
+  my_hat = int(out[1].split("=")[1])
+  my_ball = int(out[2].split("=")[1])
+  part_book = int(out[3].split("=")[1])
+  part_hat = int(out[4].split("=")[1])
+  part_ball = int(out[5].split("=")[1])
+
+  my_reward = (my_book * int(my_input[1])) + (my_hat * int(my_input[3])) + (my_ball * int(my_input[5]))
+  part_reward = (part_book * int(part_input[1])) + (part_hat * int(part_input[3])) + (part_ball * int(part_input[5]))
+
+  reward = {
+    0: int(my_reward),
+    1: int(part_reward)
+  }
+
+  return reward
+
+def determine_validity(data):
+  out = data['output']
+  my_input = data['input']
+
+  total_book = int(my_input[0])
+  total_hat = int(my_input[2])
+  total_ball = int(my_input[4])
+
+  my_book = int(out[0].split("=")[1])
+  my_hat = int(out[1].split("=")[1])
+  my_ball = int(out[2].split("=")[1])
+  part_book = int(out[3].split("=")[1])
+  part_hat = int(out[4].split("=")[1])
+  part_ball = int(out[5].split("=")[1])
+
+  if (my_book + part_book != total_book):
+    return False
+  elif (my_hat + part_hat != total_hat):
+    return False
+  elif (my_ball + part_ball != total_ball):
+    return False
   else:
-    items = [{}, {}]
-  event_list.append( populate_event(0, 'select', items[0]) )
-  event_list.append( populate_event(1, 'select', items[1]) )
-  return event_list
+    return True
 
-def process_data(examples):
-  ready = []
-  counter = 0
-  for example in examples:
-    cleaned = {}
-    cleaned['kbs'] = [ make_kb(example['input']), make_kb(example['partner']) ]
-    assert cleaned['kbs'][0][2]['Count'] == cleaned['kbs'][1][2]['Count']
-    cleaned['outcome'] = find_results(example['output'], cleaned['kbs'])
-    cleaned['events'] = parse_events(example['dialogue'], cleaned['outcome'])
-    ready.append( fit_into_transcripts(cleaned, counter) )
-    counter += 1
-  return ready
-
-def process_args():
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--split-type', default='test',
-    help='Path to the file containing the raw dialog input')
-  parser.add_argument('--output', default='transformed',
-    help='Path to the output JSON scenario file')
-  return parser.parse_args()
-
-def extract_examples(in_path, out_path):
-  examples = []
-  with open(in_path) as file:
-    for line in file:
-      example = {'input': [], 'dialogue': [], 'output': [], 'partner': []}
-      tokens = line.strip().split()
-      stage = 'input'
-      for token in tokens:
-        example[stage].append(token)
-        if token == "</input>":
-          stage = 'dialogue'
-        elif token == "</dialogue>":
-          stage = 'output'
-        elif token == "</output>":
-          stage = 'partner'
-      examples.append(example)
-  print "Extracted {} examples from {}".format(len(examples), in_path)
-  return process_data(examples)
+def process_lines(parsed):
+  cleaned = []
+  for data in parsed:
+    uuid = generate_uuid("E")
+    scenario_id = generate_uuid("FB")
+    world = {
+      "uuid": uuid,
+      "scenario": create_scenario(data, scenario_id),
+      "agents_info": {"config": [8, 5, 10]},
+      "scenario_uuid": scenario_id,
+      "agents": {0: "human", 1: "human"},
+      "outcome": create_outcome(data),
+    }
+    world["events"] = populate_events(data["dialogue"], world["outcome"]['item_split'][0])
+    cleaned.append(world)
+  return cleaned
 
 if __name__ == "__main__":
-  args = process_args()
-  in_path = 'data/{}.txt'.format(args.split_type)
-  out_path = 'data/{0}{1}.json'.format(args.output, args.split_type)
-  ready = extract_examples(in_path, out_path)
-  json.dump(ready, open(out_path, "w"))
+  for split in ["val", "test", "train"]:
+    in_filename = "{}.txt".format(split)
+    out_filename = "transformed_{}.json".format(split)
+
+    raw_in = read_file(in_filename)
+    parsed = parse_lines(raw_in)
+    cleaned = process_lines(parsed)
+    store_lines(out_filename, cleaned)
