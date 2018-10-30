48bb83c5f47f87388391045d3b5fe7befafd2978
He He <hhe.xiy@gmail.com>
2018-10-29 22:01:34 -0700

    more cleanup...

--- cocoa/model/preprocess.py
+++
@@ -1,9 +0,0 @@
-'''
-Preprocess examples in a dataset and generate data for models.
-'''
-
-import src.config as config
-import importlib
-task_module = importlib.import_module('.'.join(('src.model', config.task)))
-add_data_generator_arguments = task_module.add_data_generator_arguments
-get_data_generator = task_module.get_data_generator
